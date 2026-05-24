d8745f0389a7ac9074d42317ca2cc7d8eeeae72b
nick7-lab <scuolanick7@gmail.com>
2026-05-25 01:14:21 +0200

    Aggiunto il componente body dell'applicazione su App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import {StyleSheet, View, Text, Button, ScrollView, Image} from 'react-native'
+import {StyleSheet, View, Text, TouchableOpacity, ScrollView, Image} from 'react-native'
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context'
 import React, {useState} from 'react'
+import sfortuneList from './card.js'
 
 /**
  * Componente che renderizza l'intestazione dell'applicazione.
@@ -20,16 +21,74 @@ const Header = () => {
 }
 
 /**
+ * Componente che gestisce il testo principale del gioco in base allo stato attuale.
+ *
+ * @component
+ * @param {Object} props - Le proprietà del componente.
+ * @param {boolean} props.start - Indica se il gioco è avviato (true) o deve ancora iniziare (false).
+ * @returns {React.JSX.Element} Il testo del corpo del gioco.
+ */
+const Body = ({start, setStart, sfortunaCasuale}) => {
+
+  if (start == false){
+    return(
+      <View> 
+        <View style = {{marginBottom: 70}}>
+          <Text style = {styles.bodyText}>
+            Inizia game: 
+          </Text>
+        </View>
+
+        <View style = {{alignItems: 'center', marginBottom: 45}}>
+          <TouchableOpacity 
+            style={styles.bodyCustomButton1} 
+            onPress={() => setStart(true)}
+            activeOpacity={0.7} 
+          >
+          <Text style={styles.bodyCustomButtonText1}>START</Text>
+          </TouchableOpacity>
+        </View>
+        
+        <View style = {styles.card} key = {sfortuneList[sfortunaCasuale].id}>
+          <Image
+            source = {sfortuneList[sfortunaCasuale].immagine}
+            style = {{height: 180, width: '100%'}}
+          />
+        </View>
+      </View>
+    )
+  }
+  else {
+    return(
+      <View>
+        <TouchableOpacity 
+          onPress={() => setStart(false)}
+          activeOpacity={0.7} 
+        >
+        <Text> Ricomincia </Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+  
+}
+
+/**
  * Componente principale dell'app React Native.
  *
  * @component
  * @returns {React.JSX.Element} Il punto di ingresso principale dell'interfaccia dell'app.
  */
 export default function App() {
+  const [start, setStart] = useState(false)
+  const sfortunaCasuale = Math.floor(Math.random() * (50) + 1)
+  console.log(sfortunaCasuale)
+
   return(
     <SafeAreaProvider>
       <SafeAreaView style = {styles.containerMaster}>
         <Header/>
+        <Body start = {start} setStart = {setStart} sfortunaCasuale = {sfortunaCasuale} />
       </SafeAreaView>
     </SafeAreaProvider>
   )
@@ -37,14 +96,45 @@ export default function App() {
 
 const styles = StyleSheet.create({
   containerMaster: {
-    flex: 1
+    flex: 1,
   },
   headerView: {
-    borderWidth: 1
+    borderWidth: 1,
+    marginBottom: 9,
+    paddingVertical: 3
   },
   headerText: {
     fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center'
-  }
+  },
+  bodyText: {
+    textAlign: 'center',
+    fontSize: 18,
+  },
+  bodyCustomButton1: {
+    backgroundColor: '#ff5722', 
+    paddingVertical: 15,        
+    paddingHorizontal: 50,      
+    borderRadius: 25,           
+    shadowColor: '#000',        
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,               
+    width: '60%',               
+  },
+  bodyCustomButtonText1: {
+    color: '#ffffff',           
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',        
+    textTransform: 'uppercase'  
+  },
+  card: {
+    alignItems: 'center',
+    backgroundColor: '#F2F4F8',
+    borderRadius: 20,
+    
+  },
 })
