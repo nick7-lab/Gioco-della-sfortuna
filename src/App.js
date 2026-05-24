b7b91c7b16a7a50d77f04bd37cd6b461388f2003
nick7-lab <scuolanick7@gmail.com>
2026-05-24 15:00:50 +0200

    Aggiunta funzione main e StyleSheet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {StyleSheet, View, Text, Button, ScrollView, Image} from 'react-native'
 import {SafeAreaProvider, SafeAreaContext} from 'react-native-safe-area-context'
 import React, {useState} from 'react'
-import listaCard from './card';
 
+export default function App() {}
 
+const styles = StyleSheet.create({})
