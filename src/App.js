5198cd26bd15c5941757b437426a826d2196a681
nick7-lab <scuolanick7@gmail.com>
2026-05-24 14:49:51 +0200

    importate librerie di base per App.js

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import {StyleSheet, View, Text, Button, ScrollView, Image} from 'react-native'
+import {SafeAreaProvider, SafeAreaContext} from 'react-native-safe-area-context'
+import React, {useState} from 'react'
+import listaCard from './card';
+
+
