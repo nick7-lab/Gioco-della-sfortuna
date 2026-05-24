4f4becb0b2f7c51dc37c54078fe91f9c691ea4f6
nick7-lab <scuolanick7@gmail.com>
2026-05-24 15:30:02 +0200

    aggiunto il componente header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import {StyleSheet, View, Text, Button, ScrollView, Image} from 'react-native'
-import {SafeAreaProvider, SafeAreaContext} from 'react-native-safe-area-context'
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context'
 import React, {useState} from 'react'
 
-export default function App() {}
+/**
+ * Componente che renderizza l'intestazione dell'applicazione.
+ * Mostra il titolo del gioco centrato all'interno di un box con bordo.
+ *
+ * @component
+ * @returns {React.JSX.Element} L'elemento Header per la parte superiore dello schermo.
+ */
+const Header = () => {
+  return(
+    <View style = {styles.headerView}>
+    <Text style = {styles.headerText} >
+      Gioco della sfortuna
+    </Text>
+  </View>
+  )
+}
 
-const styles = StyleSheet.create({})
+/**
+ * Componente principale dell'app React Native.
+ *
+ * @component
+ * @returns {React.JSX.Element} Il punto di ingresso principale dell'interfaccia dell'app.
+ */
+export default function App() {
+  return(
+    <SafeAreaProvider>
+      <SafeAreaView style = {styles.containerMaster}>
+        <Header/>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  )
+}
+
+const styles = StyleSheet.create({
+  containerMaster: {
+    flex: 1
+  },
+  headerView: {
+    borderWidth: 1
+  },
+  headerText: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+})
